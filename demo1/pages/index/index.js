41bdcc0ce84ab22b242f6a0b23e974b97db3ca08
Labster22 <962739741@qq.com>
2019-03-13 23:28:40 +0800

    demo1

---
+++ demo1/pages/index/index.js
@@ -0,0 +1,22 @@
+Page({
+  data: {
+    imgUrls: [
+      'https://i.loli.net/2019/03/12/5c87a3050362c.jpg',
+      'https://i.loli.net/2019/03/12/5c87a304257e3.jpg',
+      'https://i.loli.net/2019/03/12/5c87a3032bc56.jpg',
+      'https://i.loli.net/2019/03/12/5c87a2f3b3051.jpg',
+      'https://i.loli.net/2019/03/12/5c87a2fe74dfc.jpg',
+      'https://i.loli.net/2019/03/12/5c87a3031ad93.jpg',
+      'https://i.loli.net/2019/03/12/5c87a3cf56763.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 1000
+  },
+  changeAutoplay(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+})
